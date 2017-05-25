2650a9fd4e19dcc3c3cb1eca2c6ddb7b5e0c741d
Simon Böhm <simonboehm@mailbox.org>
2017-05-25 10:17:39 +0200

    Specify html parser for beautiful soup
    Use the parser included in python's standard library

--- readmereader/__init__.py
+++ readmereader/__init__.py
@@ -63,7 +63,7 @@ def markdown_to_text(markdown_string):
     html = re.sub(r'<code>(.*?)</code >', ' ', html)
 
     # extract text
-    soup = BeautifulSoup(html)
+    soup = BeautifulSoup(html, "html.parser")
     text = ''.join(soup.findAll(text=True))
 
     return text
